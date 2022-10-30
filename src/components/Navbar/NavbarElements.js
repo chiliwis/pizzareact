2d36d5534aac5cb2357536f5ddf59f0ea2e125ba
Cristina SG <cristinasanchezg@gmail.com>
2022-10-30 16:45:56 -0600

    notworking

---
+++ src/components/Navbar/NavbarElements.js
@@ -0,0 +1,13 @@
+import { findByLabelText } from "@testing-library/react";
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+    background: transparent;
+    height: 80px;
+    display: flex;
+    justify-content: center
+    font-weight: 700;
+
+`;
+
+
